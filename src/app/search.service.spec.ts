4a74bbcc5d8c3074674c8d17ee27120cf30f5b53
Benjamin Tyszka <benjamintyszka@gmail.com>
2018-08-31 15:27:00 -0400

    resolve issue #1

--- src/app/search.service.spec.ts
+++ src/app/search.service.spec.ts
@@ -1,15 +1,35 @@
-import { TestBed, inject } from '@angular/core/testing'
-
 import { SearchService } from './search.service'
+import { IContent } from './content'
+import { defer } from 'rxjs'
+
+export function asyncData<T>(data: T) {
+    return defer(() => Promise.resolve(data))
+}
 
 describe('SearchService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [SearchService]
+    let service: SearchService
+    let httpClientSpy: { get: jasmine.Spy }
+    beforeEach(() => {
+        httpClientSpy = jasmine.createSpyObj('HttpClient', ['get'])
+        service = new SearchService(<any>httpClientSpy)
     })
-  })
 
-  it('should be created', inject([SearchService], (service: SearchService) => {
-    expect(service).toBeTruthy()
-  }))
+    it('should return movie on valid query', () => {
+        const expectedMovie: IContent = {
+            Country: 'USA',
+            Title: 'Test',
+            Director: 'TEst',
+            Genre: 'Test',
+            Metascore: 'Test',
+            Plot: 'Test',
+            Poster: 'Test',
+            Production: 'Test',
+            Released: 'Test',
+        }
+        httpClientSpy.get.and.returnValue(asyncData(expectedMovie))
+        service.search('Test').subscribe(
+            data => expect(data).toEqual(expectedMovie, 'expected movie'),
+            fail
+        )
+    })
 })
--- src/app/search/search.component.spec.ts
+++ src/app/search/search.component.spec.ts
@@ -1,4 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing'
+import { ReactiveFormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatCardModule, MatFormFieldModule, MatInputModule, MatToolbarModule, MatButtonModule, MatIconModule } from '@angular/material'
 
 import { SearchComponent } from './search.component'
 
@@ -8,18 +12,57 @@ describe('SearchComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SearchComponent ]
+      declarations: [SearchComponent],
+      imports:
+        [
+          ReactiveFormsModule,
+          BrowserAnimationsModule,
+          HttpClientModule,
+          MatCardModule,
+          MatFormFieldModule,
+          MatInputModule,
+          MatToolbarModule,
+          MatButtonModule,
+          MatIconModule,
+        ]
     })
-    .compileComponents()
+      .compileComponents()
   }))
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SearchComponent)
     component = fixture.componentInstance
+
     fixture.detectChanges()
+    component.ngOnInit()
   })
 
   it('should create', () => {
     expect(component).toBeTruthy()
   })
+
+  it('form should be disabled to start', () => {
+    expect(component.form.valid).toBeFalsy()
+  })
+
+  it('form should be disabled if less than 3 characters', () => {
+    component.form.get('query').setValue('ee')
+    expect(component.form.valid).toBeFalsy()
+  })
+
+  it('form should be disabled if more than 24 characters', () => {
+    component.form.get('query').setValue('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee')
+    expect(component.form.valid).toBeFalsy()
+  })
+
+  it('form should be disabled if strange characters are entered', () => {
+    component.form.get('query').setValue('B)n *% Ko*>')
+    expect(component.form.valid).toBeFalsy()
+  })
+
+  it('form should be enabled if normal input', () => {
+    component.form.get('query').setValue('Star Wars')
+    expect(component.form.valid).toBeTruthy()
+  })
+
 })
